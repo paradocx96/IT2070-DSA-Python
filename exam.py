2108adf99d864f693299f9e05b679fbfef0507c4
Paradocx96 <adilanga@gmail.com>
2021-02-16 10:18:13 +0530

    Updated!

---
+++ exam.py
@@ -0,0 +1,16 @@
+#Getting Keyboad input
+n = int(input("Enter a number :"))
+
+# Implement sumcube function
+def sumcube(n):
+    if n == 1: # check the value is 1
+        return 1
+    else:
+        return (sumcube(n-1) + (n * n * n)) #calsulation
+
+# Run the program
+while (n != -1): # Check number is equal to -1
+    #Call the sumcube function
+    print("Result :", sumcube(n))
+    # Getting next input
+    n = int(input("Enter a number :"))
